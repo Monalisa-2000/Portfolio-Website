939c3299c7d448a7e12e0a45dc5ba637910d55e7
Monalisa-2000 <75751123+Monalisa-2000@users.noreply.github.com>
2022-09-09 03:51:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function btn_click() {
+    window.location.href = "https://www.linkedin.com/in/monalisa-khanda-287631187/";
+}
+function click_PAA() {
+    window.location.href = "https://github.com/Monalisa-2000/Productivity_App/";
+}
+function click_BCP() {
+    window.location.href = "https://github.com/Monalisa-2000/Breast_Cancer_Prediction/";
+}
